d70c963440169538049c8ccb9641e4c824bb3c27
King Larce Cortes <kingcort01@gmail.com>
2022-01-27 08:51:46 +0800

    Add number of 1 bits

---
+++ 12-number-of-1-bits.py
@@ -0,0 +1,48 @@
+"""
+Problem:
+Write a function that takes an unsigned integer and returns the number of '1' bits it has (also known as the Hamming weight).
+Example 1:
+
+Input: n = 00000000000000000000000000001011
+Output: 3
+Explanation: The input binary string 00000000000000000000000000001011 has a total of three '1' bits.
+Example 2:
+
+Input: n = 00000000000000000000000010000000
+Output: 1
+Explanation: The input binary string 00000000000000000000000010000000 has a total of one '1' bit.
+Example 3:
+
+Input: n = 11111111111111111111111111111101
+Output: 31
+Explanation: The input binary string 11111111111111111111111111111101 has a total of thirty one '1' bits.
+"""
+# Just a debugging function for checking the binary of the number
+def decimalToBinary(num):
+  if num >= 1:
+      decimalToBinary(num // 2)
+      print(num % 2)
+
+# Brute force: O(1)
+def count_bits(n):
+  print("curren ; ", n, " - ", decimalToBinary(n))
+  print("shifted: ", n>>1, " - ", decimalToBinary(n>>1))
+
+  result = 0
+  while n:
+      result += n % 2 # This give 1 or 0 and just add up to result
+      n = n>>1
+      
+  return result
+
+# Better solution: O(1)
+def count_bits(n):
+  result = 0
+        
+  while n != 0:
+      result += 1
+      n = n & (n-1)
+      
+  return result
+
+print(count_bits(11)) # Expected output 3
