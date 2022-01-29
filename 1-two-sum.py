6bfb1d30e8f7177ddcf1501ca58e4fed6826ac7c
King Larce Cortes <kingcort01@gmail.com>
2022-01-29 13:44:29 +0800

    updated two sum

--- 1-two-sum.py
+++ 1-two-sum.py
@@ -17,15 +17,34 @@
 
 # input: [1,30,21,4,6,60], 81
 # output: [21,60]
-def two_sum(arr, total_sum):
+def two_sum(nums, target):
   uniq = {}
-  for i in range(len(arr)): 
-    num_diff = abs(total_sum-arr[i])
+  for i in range(len(nums)): 
+    num_diff = abs(target-nums[i])
     if not num_diff in uniq:
-      uniq[arr[i]] = True
+      uniq[nums[i]] = True
     else:
-      return [arr[i], num_diff]
+      return [nums[i], num_diff]
   return False
+
+def two_sum(nums, target):
+  for i in range(len(nums)):
+    for j in range(i + 1, len(nums)):
+      if (nums[i]+nums[j]) == target:
+          return [i, j]
+
+def two_sum(nums, target):
+    left, right = 0, len(nums)-1
+    while left < right:
+        total = nums[left]+nums[right]
+        if total == target:
+            return [left+1, right+1]
+        elif total < target:
+            left += 1
+        else:
+            right -= 1
+            
+    return -1
 
 print(two_sum([1,30,21,4,6,60], 81))
 
