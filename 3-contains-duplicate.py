06c3674955f50d38730ed7c43331a85a01a8a4c9
King Larce Cortes <kingcort01@gmail.com>
2022-01-31 12:31:37 +0800

    Updated 3 & 4

--- 3-contains-duplicate.py
+++ 3-contains-duplicate.py
@@ -12,22 +12,26 @@
 # input: [1,2,3,1]
 # output: true
 
-def contains_duplicate(arr):
-  left = 0
-  right = 1
-  while left < len(arr)-1:
-    left_value = arr[left]
-    right_value = arr[right]
-    if left_value == right_value:
-      return False
-    right += 1
-    if right >= len(arr):
-      left += 1
-      right = left + 1
+def contains_duplicate(nums):
+    nums.sort()
+    for i in range(len(nums)-1):
+        if nums[i] == nums[i+1]:
+            return True
+    return False
 
-  return True
+def contains_duplicate(nums):
+    nums_set = list(set(nums))
+    return not (len(nums_set) == len(nums))
+
+def contains_duplicate(nums):
+    uniq_nums = set()
+    for num in nums:
+        if num in uniq_nums:
+            return True
+        uniq_nums.add(num)
+    return False
 
 print(contains_duplicate([1,2,3,4,1]))
-print(contains_duplicate([1,2,3,4]))
-print(contains_duplicate([1,1,1,3,3,4,3,2,4,2]))
+# print(contains_duplicate([1,2,3,4]))
+# print(contains_duplicate([1,1,1,3,3,4,3,2,4,2]))
 
--- 4-product-array-except-self.py
+++ 4-product-array-except-self.py
@@ -21,34 +21,34 @@ list1 = [1,2,3,4]
 # output [24,12,8,6]
 
 # using running pointer - t: o(n), s: o(1)
-# def product_array_except_self(arr):
-#   result = [1 for _ in range(len(arr))]
-#   left = 0
-#   right = 0
-#   result[left] = arr[left]
-#   while left < len(arr):
-#     right_value = arr[right]
-#     if right != left:
-#       result[left] = result[left] * right_value
-#     right += 1
-#     if right >= len(arr):
-#       left += 1
-#       right = 0
-#   return result
+def product_array_except_self(nums):
+    result = [1] * (len(nums))
+    left = 0
+    right = 1
+    result[left] = nums[left]
+    while left < len(nums):
+        right_value = nums[right]
+        if right != left:
+            result[left] = result[left] * right_value
+        right += 1
+        if right >= len(nums):
+            left += 1
+            right = 0
+    return result
 
-def product_array_except_self(arr):
-  result = [1] * (len(arr))
-  prefix = 1
-  for i in range(len(arr)):
-    result[i] = prefix
-    prefix = prefix * arr[i]
-  postfix = 1
-  for i in range(len(arr)-1, -1, -1):
-    result[i] = result[i] * postfix
-    postfix = postfix * arr[i]
+def product_array_except_self(nums):
+    result = [1] * (len(nums))
+    prefix = 1
+    for i in range(len(nums)):
+        result[i] = prefix
+        prefix = prefix * nums[i]
+    postfix = 1
+    for i in range(len(nums)-1, -1, -1):
+        result[i] = result[i] * postfix
+        postfix = postfix * nums[i]
 
-  return result
+    return result
 
-
-print(product_array_except_self(list1))
+print(product_array_except_self([1,2,3,4])) # Expect [24,12,8,6]
+print(product_array_except_self([-1,1,0,-3,3])) # Expect [0,0,9,0,0]
 
